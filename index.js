aa30a19eb46b359ba1a589fd20906208d810a1dd
Steamgx <your-bmuraya646@gmail.com>
2024-11-25 22:16:36 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code in this file!
+ 
+const currentUser = 'Hopper';
+  
+  
+  const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+  
+  
+  const excitedWelcomeMessage = `WELCOME TO FLATBOOK, HOPPER!`;
+  
+  
+  const shortGreeting = `Welcome, H!`;
+  
+  console.log(welcomeMessage); 
+  console.log(excitedWelcomeMessage); 
+  console.log(shortGreeting); 
+  
+
+
